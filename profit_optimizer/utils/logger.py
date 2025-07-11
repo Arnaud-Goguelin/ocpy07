9b3e686b5ffb5adfeec8cfa776eae97c8f7e3fcf
Arnaud GOGUELIN <arnaud.goguelin@gmail.com>
2025-07-11 11:54:02 +0200

    stle(logger): reformat file to match with flake8 recommendations

--- profit_optimizer/utils/logger.py
+++ profit_optimizer/utils/logger.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format=f"%(asctime)s - %(levelname)s - %(message)s",
+    format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%H:%M:%S",
     handlers=[
         # display in terminal
